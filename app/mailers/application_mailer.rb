092d7f23e13e8ddafbf628b40e44e87bebe139d1
julio336 <julio336@hotmail.com>
2021-02-09 22:00:18 -0600

    adding api call

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,5 @@
+require "net/http"
+
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
@@ -5,8 +7,12 @@ class ApplicationMailer < ActionMailer::Base
   def test_email()
 		puts "Email enviado"
       	to = "julio336@hotmail.com"
-
-	     mail(:to => to, :subject => "test email", :from => "default_sender@foo.bar") do |format|
+      	url = "https://api.taapi.io/rsi?secret=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1bGlvMzM2QGhvdG1haWwuY29tIiwiaWF0IjoxNjEyOTI4MzUyLCJleHAiOjc5MjAxMjgzNTJ9.zIZbSk9qMs6-P9OMGyS_c2kO2vdxWIXio6c3nrYh6UE
+				&exchange=binance&symbol=BTC/USDT&interval=15m"
+		resp = Net::HTTP.get_response(URI.parse(url))
+		data = JSON.parse(resp.body)
+		puts data
+	    mail(:to => to, :subject => data, :from => "default_sender@foo.bar") do |format|
         		format.text(:content_type => "text/plain", :charset => "UTF-8", :content_transfer_encoding => "7bit")
         end
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,12 @@ class UserMailer < ApplicationMailer
 	def test_email()
 		puts "Email enviado"
       	to = "julio336@hotmail.com"
-
-	     mail(:to => to, :subject => "test email", :from => "default_sender@foo.bar") do |format|
-        		format.text(:content_type => "text/plain", :charset => "UTF-8", :content_transfer_encoding => "7bit")
-        end
+      	url = "https://api.taapi.io/rsi?secret=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1bGlvMzM2QGhvdG1haWwuY29tIiwiaWF0IjoxNjEyOTI4MzUyLCJleHAiOjc5MjAxMjgzNTJ9.zIZbSk9qMs6-P9OMGyS_c2kO2vdxWIXio6c3nrYh6UE
+				&exchange=binance&symbol=BTC/USDT&interval=1h"
+		data = JSON.parse(open(url).read)
+		puts data
+	    #mail(:to => to, :subject => "test email", :from => "default_sender@foo.bar") do |format|
+        #		format.text(:content_type => "text/plain", :charset => "UTF-8", :content_transfer_encoding => "7bit")
+        #end
     end
 end
