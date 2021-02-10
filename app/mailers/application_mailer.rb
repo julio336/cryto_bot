53b8c558ea3c204f4a0a569cfe07c67fff6ecfdb
julio336 <julio336@hotmail.com>
2021-02-09 22:26:41 -0600

    rsi

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,15 +5,20 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
 
   def test_email()
-		puts "Email enviado"
       	to = "julio336@hotmail.com"
       	url = "https://api.taapi.io/rsi?secret=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1bGlvMzM2QGhvdG1haWwuY29tIiwiaWF0IjoxNjEyOTI4MzUyLCJleHAiOjc5MjAxMjgzNTJ9.zIZbSk9qMs6-P9OMGyS_c2kO2vdxWIXio6c3nrYh6UE
 				&exchange=binance&symbol=BTC/USDT&interval=15m"
 		resp = Net::HTTP.get_response(URI.parse(url))
 		data = JSON.parse(resp.body)
-		puts data
-	    mail(:to => to, :subject => data, :from => "default_sender@foo.bar") do |format|
+
+		if data['value'] <= 30
+			puts "Email enviado"
+			puts data['value']
+	    	mail(:to => to, :subject => data, :from => "default_sender@foo.bar") do |format|
         		format.text(:content_type => "text/plain", :charset => "UTF-8", :content_transfer_encoding => "7bit")
+        	end
+        else
+        	puts data
         end
     end
 end
