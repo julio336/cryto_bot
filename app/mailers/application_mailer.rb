872da2c860e857835d604f8e149b35e1f45f746a
julio336 <julio336@hotmail.com>
2021-02-10 20:22:02 -0600

    mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,24 +1,12 @@
-require "net/http"
 
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 
-  def test_email()
-      	to = "julio336@hotmail.com"
-      	url = "https://api.taapi.io/rsi?secret=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1bGlvMzM2QGhvdG1haWwuY29tIiwiaWF0IjoxNjEyOTI4MzUyLCJleHAiOjc5MjAxMjgzNTJ9.zIZbSk9qMs6-P9OMGyS_c2kO2vdxWIXio6c3nrYh6UE
-				&exchange=binance&symbol=BTC/USDT&interval=15m"
-		resp = Net::HTTP.get_response(URI.parse(url))
-		data = JSON.parse(resp.body)
-
-		if data['value'] <= 30
-			puts "Email enviado"
-			puts data['value']
-	    	mail(:to => to, :subject => data, :from => "default_sender@foo.bar") do |format|
-        		format.text(:content_type => "text/plain", :charset => "UTF-8", :content_transfer_encoding => "7bit")
-        	end
-        else
-        	puts data
-        end
+  def test_email(subject)
+    to = "julio336@hotmail.com"
+    mail(:to => to, :subject => subject, :from => "default_sender@foo.bar") do |format|
+    	format.text(:content_type => "text/plain", :charset => "UTF-8", :content_transfer_encoding => "7bit")
+		end			
     end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,3 +1,5 @@
+require "net/http"
+
 namespace :scheduled_tasks do
   desc "Sync with some source"
   task update: :environment do
@@ -11,7 +13,22 @@ namespace :scheduled_tasks do
       puts "Number of Things not yet completed is"
   end
   task :mailme => :environment do
-    ApplicationMailer.test_email.deliver
+    crypto_pair = {"btc" => "BTC/USDT", "eth" => "ETH/USDT", "xrp" => "XRP/USDT", "ltc" => "LTC/USDT",
+                 "xmr" => "XMR/USDT"}
+        
+    crypto_pair.each do |crypto, pair|
+      url = "https://api.taapi.io/rsi?secret=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1bGlvMzM2QGhvdG1haWwuY29tIiwiaWF0IjoxNjEzMDA4ODgyLCJleHAiOjc5MjAyMDg4ODJ9.Kuut9k7NMH-TPQQmV6YdjgmYyH7wlGR4ZQmB8x1WhTA&exchange=binance&symbol=#{pair}&interval=15m"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      data = JSON.parse(resp.body)
+      if data['value'] <= 50
+        puts "Email enviado"
+        subject = "#{pair}: #{data['value']}"
+        puts subject
+        ApplicationMailer.test_email(subject).deliver
+      else
+        puts data
+      end
+    end
   end
 end
 
