b28eb8d53fbd8bae80418cd0e67e4acb2f7a11b9
julio336 <julio336@hotmail.com>
2021-07-26 13:57:35 -0500

    modify

--- sheet.rb
+++ sheet.rb
@@ -10,10 +10,15 @@ crypto_pair.each do |crypto, pair|
 	url_candle = "https://api.taapi.io/candle?secret=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1bGlvMzM2QGhvdG1haWwuY29tIiwiaWF0IjoxNjEzMDA4ODgyLCJleHAiOjc5MjAyMDg4ODJ9.Kuut9k7NMH-TPQQmV6YdjgmYyH7wlGR4ZQmB8x1WhTA&exchange=binance&symbol=#{pair}&interval=15m"
 	resp_candle = Net::HTTP.get_response(URI.parse(url_candle))
     data_candle = JSON.parse(resp_candle.body)
-    puts data_candle
+    #puts data_candle
     data_candle.each do |i,hash|
 		#puts i
-		crypto_arr << hash
+		count = 0
+		if count == 0
+			crypto_arr << hash
+		else
+			crypto_arr << hash.to_i
+		end
 		#puts hash
 	end
     puts crypto_arr
